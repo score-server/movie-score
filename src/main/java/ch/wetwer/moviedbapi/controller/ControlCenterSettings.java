7dbd7a70d08c9a97f36398e42172bce3ee206193
Wetwer <felix.spoerri@gmail.com>
2021-02-22 15:29:17 +0100

    Added Converting for Movies Matroska/AVI to MP4

--- src/main/java/ch/wetwer/moviedbapi/controller/ControlCenterSettings.java
+++ src/main/java/ch/wetwer/moviedbapi/controller/ControlCenterSettings.java
@@ -4,6 +4,7 @@ package ch.wetwer.moviedbapi.controller;
 import ch.wetwer.moviedbapi.data.activitylog.ActivityLogDao;
 import ch.wetwer.moviedbapi.data.episode.EpisodeDao;
 import ch.wetwer.moviedbapi.data.importlog.ImportLogDao;
+import ch.wetwer.moviedbapi.data.movie.MovieDao;
 import ch.wetwer.moviedbapi.data.request.RequestDao;
 import ch.wetwer.moviedbapi.service.auth.UserAuthService;
 import ch.wetwer.moviedbapi.service.filehandler.FileHandler;
@@ -16,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 
 /**
  * @author Wetwer
@@ -31,34 +31,26 @@ public class ControlCenterSettings {
     private ActivityLogDao activityLogDao;
     private RequestDao requestDao;
     private EpisodeDao episodeDao;
+    private MovieDao movieDao;
 
     private SettingsService settingsService;
     private UserAuthService userAuthService;
 
     public ControlCenterSettings(SettingsService settingsService, UserAuthService userAuthService,
                                  ImportLogDao importLogDao, ActivityLogDao activityLogDao, RequestDao requestDao,
-                                 EpisodeDao episodeDao) {
+                                 EpisodeDao episodeDao, MovieDao movieDao) {
         this.settingsService = settingsService;
         this.userAuthService = userAuthService;
         this.importLogDao = importLogDao;
         this.activityLogDao = activityLogDao;
         this.requestDao = requestDao;
         this.episodeDao = episodeDao;
+        this.movieDao = movieDao;
     }
 
     @GetMapping
     private String getControlCenter(Model model, HttpServletRequest request) {
         if (userAuthService.isAdministrator(model, request)) {
-
-            model.addAttribute("freeSpace",
-                    new File(settingsService.getKey("moviePath")).getTotalSpace() /
-                            new File(settingsService.getKey("moviePath")).getUsableSpace() * 100);
-
-            System.out.println((new File(settingsService.getKey("moviePath")).getUsableSpace() /
-                    new File(settingsService.getKey("moviePath")).getTotalSpace()) * 100);
-            System.out.println(new File(settingsService.getKey("moviePath")).getTotalSpace() + " free " +
-                    new File(settingsService.getKey("moviePath")).getUsableSpace());
-
             model.addAttribute("moviePath", settingsService.getKey("moviePath"));
             model.addAttribute("seriePath", settingsService.getKey("seriePath"));
             model.addAttribute("previewPath", settingsService.getKey("preview"));
@@ -158,7 +150,9 @@ public class ControlCenterSettings {
     public String getConvertProgress(Model model, HttpServletRequest request) {
         if (userAuthService.isAdministrator(model, request)) {
             model.addAttribute("episodes", episodeDao.getEpisodesCurrentlyConverting());
+            model.addAttribute("movies", movieDao.getMoviesCurrentlyConverting());
             model.addAttribute("episodesToConvert", episodeDao.getEpisodesToConvert());
+            model.addAttribute("moviesToConvert", movieDao.getMoviesToConvert());
             model.addAttribute("page", "convert");
             return "template";
         } else {
